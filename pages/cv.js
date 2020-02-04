3c72cdae37b5c367f2b41c5e683a358db826610f
Maxwell Clarke (Maxeonyx) <maxeonyx@gmail.com>
2020-02-04 20:55:14 +1300

    Initial commit. Setup Next.js

---
+++ pages/cv.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+import Header from '../components/header'
+
+export default () => (
+    <div>
+        <Header></Header>
+        <p>My CV</p>
+    </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+import Header from '../components/header'
+
+export default () => (
+    <div>
+        <Header></Header>
+        <p>Home page</p>
+    </div>
+)
